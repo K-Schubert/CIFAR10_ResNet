4f00de45a272609207c44050bb160312e4bb46a0
Kieran Schubert <schubert.kieran@gmail.com>
2020-07-21 20:23:17 +0200

    first commit

---
+++ CIFAR10_ResNet.py
@@ -0,0 +1,58 @@
+import os
+import torch
+import torchvision
+import tarfile
+import torch.nn as nn
+import numpy as np
+import torch.nn.functional as F
+from torchvision.datasets.utils import download_url
+from torchvision.datasets import ImageFolder
+from torch.utils.data import DataLoader
+import torchvision.transforms as tt
+from torch.utils.data import random_split
+from torchvision.utils import make_grid
+import matplotlib.pyplot as plt
+
+
+project_name='05b-cifar10-resnet'
+
+# Dowload the dataset
+dataset_url = "http://files.fast.ai/data/cifar10.tgz"
+download_url(dataset_url, '.')
+
+# Extract from archive
+with tarfile.open('./cifar10.tgz', 'r:gz') as tar:
+    tar.extractall(path='./data')
+    
+# Look into the data directory
+data_dir = './data/cifar10'
+print(os.listdir(data_dir))
+classes = os.listdir(data_dir + "/train")
+print(classes)
+
+# Data transforms (normalization & data augmentation)
+stats = ((0.4914, 0.4822, 0.4465), (0.2023, 0.1994, 0.2010))
+train_tfms = tt.Compose([tt.RandomCrop(32, padding=4, padding_mode='reflect'), 
+                         tt.RandomHorizontalFlip(), 
+                         tt.ToTensor(), 
+                         tt.Normalize(*stats,inplace=True)])
+valid_tfms = tt.Compose([tt.ToTensor(), tt.Normalize(*stats)])
+
+# PyTorch datasets
+train_ds = ImageFolder(data_dir+'/train', train_tfms)
+valid_ds = ImageFolder(data_dir+'/test', valid_tfms)
+
+batch_size = 400
+
+# PyTorch data loaders
+train_dl = DataLoader(train_ds, batch_size, shuffle=True, num_workers=3, pin_memory=True)
+valid_dl = DataLoader(valid_ds, batch_size*2, num_workers=3, pin_memory=True)
+
+def show_batch(dl):
+    for images, labels in dl:
+        fig, ax = plt.subplots(figsize=(12, 12))
+        ax.set_xticks([]); ax.set_yticks([])
+        ax.imshow(make_grid(images[:64], nrow=8).permute(1, 2, 0))
+        break
+
+show_batch(train_dl)
